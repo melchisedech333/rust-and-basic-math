360473194b2d7eb2aa4b1fcda5a2fa21689d2819
melchisedech333 <melchisedech333@gmail.com>
2022-10-27 21:01:50 -0300

    Updates

--- 2 - basic arithmetic/main.rs
+++ 2 - basic arithmetic/main.rs
@@ -1,7 +1,15 @@
 
 fn main() {
+    println!("Operations: \n");
+    operations();
+
+    println!("\n\n\nSignals: \n");
+    signals();
+}
+
+fn operations() {
     let a = |x,y| x + y;
-    println!("a: {}\n", a(10, 20));
+    println!("a: {}", a(10, 20));
 
     let calc = |x,y,c| {
         match c {
@@ -20,6 +28,35 @@ fn main() {
     println!("-: {}", calc(b, c, '-'));
     println!("*: {}", calc(b, c, '*'));
     println!("/: {}", calc(b, c, '/'));
+}
+
+fn signals() {
+    let show = |a,b| println!("{a} = {b}");
+
+    show(
+        (4) + (-2) + (1) - (1) ,
+         4     -2  +  1  -  1
+    );
+
+    show(
+        (-6) - (10) + (-3) ,
+         -6  -  10     -3
+    );
+
+    show(
+        (10) + (-5) - (-10) ,
+         10     -5  +   10
+    );
+
+    show(
+        (15) + (-10) - (4) + ( 20 - 30) + (-1),
+         15     -10  -  4  + (-10     )    -1
+    );
+
+    show(
+        200 + (-23) - (-30) + 78 + (50 / 100) - 10,
+        200    -23  +   30  + 78 + (50 / 100) - 10
+    );
 }
 
 
