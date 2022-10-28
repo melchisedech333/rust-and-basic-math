f67aabd0f2878088f7fe5544b00dbeaf26cf3377
melchisedech333 <melchisedech333@gmail.com>
2022-10-28 03:28:07 -0300

    Updates

--- 3 - divisibility criteria/main.rs
+++ 3 - divisibility criteria/main.rs
@@ -2,10 +2,10 @@
 use utils::*;
 
 fn main() {
-    let value = 1228;
+    let value = 4;
     println!("\nInput: {value}\n");
 
-    for number in 2..9 {
+    for number in 2..=9 {
         println!("{}: {}", number, divisible_by(value, number));
     }
 }
